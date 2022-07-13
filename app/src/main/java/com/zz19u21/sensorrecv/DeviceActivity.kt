d0c6cc0a67b6aaae4b5797c8d5c34d7092639fe6
DrakeZhao <zz19u21@soton.ac.uk>
2022-07-13 17:03:47 +0100

    added DeviceActivity.kt

---
+++ app/src/main/java/com/zz19u21/sensorrecv/DeviceActivity.kt
@@ -0,0 +1,11 @@
+package com.zz19u21.sensorrecv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DeviceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_device)
+    }
+}
