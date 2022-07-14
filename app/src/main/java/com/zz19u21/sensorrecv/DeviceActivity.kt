dd64f634e97a31c1f32a6da109630aa59c6bc0b8
DrakeZhao <zz19u21@soton.ac.uk>
2022-07-14 11:56:48 +0100

    added basic funcs

--- app/src/main/java/com/zz19u21/sensorrecv/DeviceActivity.kt
+++ app/src/main/java/com/zz19u21/sensorrecv/DeviceActivity.kt
@@ -2,10 +2,42 @@ package com.zz19u21.sensorrecv
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.zz19u21.sensorrecv.databinding.ActivityDeviceBinding
+import com.zz19u21.sensorrecv.databinding.ActivityMainBinding
 
 class DeviceActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDeviceBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_device)
+        binding = ActivityDeviceBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.topAppBar.setNavigationOnClickListener{
+            finish()
+        }
+        binding.topAppBar.setOnMenuItemClickListener{ menuItem ->
+            when (menuItem.itemId){
+                R.id.edit_device ->{
+                    Toast.makeText(this, "Edit device", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.delete_device ->{
+                    Toast.makeText(this, "Delete device", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.setDevice ->{
+                    Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.exportData -> {
+                    Toast.makeText(this, "Export Data", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+
+        }
     }
 }
--- app/src/main/java/com/zz19u21/sensorrecv/MainActivity.kt
+++ app/src/main/java/com/zz19u21/sensorrecv/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.zz19u21.sensorrecv
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.zz19u21.sensorrecv.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , View.OnClickListener{
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +15,36 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        binding.card1.setOnClickListener(this)
+        binding.card2.setOnClickListener(this)
+        binding.card3.setOnClickListener(this)
+        binding.topAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId){
+                R.id.add_bluetooth -> {
+                    Toast.makeText(this, "Scan bluetooth", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
     }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.card1 -> {
+                val intent = Intent(this, DeviceActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.card2 -> {
+                val intent = Intent(this, DeviceActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.card3 -> {
+                val intent = Intent(this, DeviceActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+
 }
