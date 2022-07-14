a6db4660818979ae3cf2ff93407b2d9b9374604d
DrakeZhao <zz19u21@soton.ac.uk>
2022-07-14 13:20:21 +0100

    Added sensor page

---
+++ app/src/main/java/com/zz19u21/sensorrecv/DataActivity.kt
@@ -0,0 +1,38 @@
+package com.zz19u21.sensorrecv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.zz19u21.sensorrecv.databinding.ActivityDataBinding
+import com.zz19u21.sensorrecv.databinding.ActivityDeviceBinding
+
+class DataActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDataBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDataBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.topAppBar.setNavigationOnClickListener{
+            finish()
+        }
+        binding.topAppBar.setOnMenuItemClickListener{ menuItem ->
+            when (menuItem.itemId){
+                R.id.shareValue ->{
+                    Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.setDevice ->{
+                    Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.delete_device->{
+                    Toast.makeText(this, "Delete", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+
+        }
+    }
+}
